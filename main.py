74fcf223dc36a3bab4118d1cce9506b74ebd8c25
Parmethyst <71919830+Parmethyst@users.noreply.github.com>
2021-08-13 06:44:34 +0700

    Added dynasty link support
    Sometimes image doesnt show though

--- main.py
+++ main.py
@@ -35,6 +35,104 @@ async def _ping(ctx):
     await ctx.send(f"Pong! ({client.latency*1000}ms)")
 
 @slash.slash( #Make a slash command
+    name="ds",
+    description="Command to preview dynasty-scans link",
+    options=[
+        create_option(
+            name="link",
+            description="Put your dynasty-scans link here",
+            option_type=3,
+            required=False
+        )
+    ])
+async def ds(ctx, link="help"): #Slash command function
+    link=link.lower()
+    ts = time.localtime()
+    timeString=time.strftime("%Y-%m-%d %H:%M:%S", ts)
+    print(f"[{timeString}] {ctx.author.display_name}: {link}")
+    if link=="help":
+        print("Usage help successfully sent")
+        await ctx.send(content=f"Usage: /ds [dynasty-scans link without the brackets]")
+        return
+    if not re.match(r"(https:\/\/dynasty-scans.com\/)(series|anthologies|issues|chapters)\/([a-z\-0-9\-_])+", link):
+        print("Not a dynasty-scans link!")
+        await ctx.send('dynasty-scans link malformed! If this is not a typo please contact Parmethyst#7954.', hidden=True)
+        return
+    await ctx.defer()
+    if link[-1]=='/': #if last character of link is slash then remove slash
+        link=link[:-1]
+    split_link=link.rsplit('/',2)[1:] #split link to parts divided by '/' character
+
+    if split_link[0]=="chapters":
+        requested_page=1
+        chapter_permalink=""
+        if re.match(r"([a-z\-0-9\-_])+#([0-9])+", split_link[1]):
+            chapter_split=split_link[1].split('#')
+            chapter_permalink=chapter_split[0]
+            requested_page=int(chapter_split[1])
+            if(requested_page<1): requested_page=1
+        else:
+            chapter_permalink=split_link[1]
+        json_link=f"https://dynasty-scans.com/chapters/{chapter_permalink}.json"
+        response = requests.get(f"{json_link}")
+        json_data = json.loads(response.text)
+        if requested_page >= len(json_data["pages"]):
+            requested_page=len(json_data["pages"])
+        title=json_data["title"]
+        page_image=json_data["pages"][requested_page-1]["url"]
+        embed=discord.Embed(
+            title=f"{title}",
+            url=f"{link}")
+        embed.add_field(name="Preview doesn't load?", value=f"[https://dynasty-scans.com{page_image}](https://dynasty-scans.com{page_image})")
+        embed.set_author(
+            name=ctx.author.display_name, 
+            icon_url=ctx.author.avatar_url)
+        embed.set_image(url=f"https://dynasty-scans.com{page_image}")
+        embed.set_footer(text="Code: https://github.com/Parmethyst/MdexPreviewBot")
+        print(f"{title}")
+        await ctx.send(embed=embed)
+
+    elif split_link[0]=="series" or split_link[0]=="anthologies" or split_link[0]=="issues":
+        is_nsfw=False
+        is_tags=False
+        response = requests.get(f"{link}.json")
+        json_data = json.loads(response.text)
+        title=json_data["name"]
+        description=""
+        if json_data["description"] is None:
+            description=""
+        else:
+            description=json_data["description"]
+        cover_url=json_data['cover'].partition("?")
+        cover_link=f"https://dynasty-scans.com{cover_url[0]}"
+        tags=json_data["tags"]
+        tags.sort(key=lambda x: x["type"], reverse=False)
+        tag_list=list()
+        for tag in tags:
+            is_tags=True
+            tag_list.append(tag["name"])
+            if tag["name"]=="NSFW":
+                is_nsfw=True
+        tag_string=", ".join(tag_list)
+        embed=discord.Embed(
+            title=f"{title}",
+            url=f"{link}",
+            description=f"{description}")
+        if is_tags: embed.add_field(name="Tags", value=f"{tag_string}")
+        embed.set_author(
+            name=ctx.author.display_name, 
+            icon_url=ctx.author.avatar_url)
+        if not is_nsfw:
+            embed.set_image(url=f"{cover_link}")
+        elif is_nsfw and ctx.channel.is_nsfw():
+            embed.set_image(url=f"{cover_link}")
+        embed.set_footer(text="Code: https://github.com/Parmethyst/MdexPreviewBot")
+        print(f"{title}")
+        await ctx.send(embed=embed)
+
+
+
+@slash.slash( #Make a slash command
     name="md",
     description="Command to preview a mangadex link",
     options=[
