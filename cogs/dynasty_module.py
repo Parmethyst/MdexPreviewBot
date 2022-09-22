a7d11ef8a814d17618c0150915055dbb4812527a
Parmethyst <syncdrome84@gmail.com>
2022-09-22 18:26:30 +0700

    Reworked Dynasty Discord.py 2.0+

---
+++ cogs/dynasty_module.py
@@ -0,0 +1,127 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+import time
+import json
+import requests
+import re
+
+
+class dynasty_module(commands.Cog):
+  def __init__(self, bot: commands.Bot) -> None:
+      self.bot = bot
+      super().__init__()  # this is now required in this context.
+
+  @app_commands.command(name="ds", description="Command to preview dynasty-scans link")
+  async def dynasty(self, interaction: discord.Interaction,
+    link: str) -> None:
+      """ /commands1 """
+      link = link.lower()
+      ts = time.localtime()
+      timeString=time.strftime("%Y-%m-%d %H:%M:%S", ts)
+      print(f"[{timeString} | {interaction.user}]: {link}")
+      if link=="help":
+        print("Usage help successfully sent")
+        await interaction.response.send_message(content=f"Usage: /ds [dynasty-scans link without the brackets]",ephemeral=True)
+        return
+      if not re.match(r"(https:\/\/dynasty-scans.com\/)(series|anthologies|issues|chapters)\/([a-z\-0-9\-_])+", link):
+        print("Not a dynasty-scans link!")
+        await interaction.response.send_message('dynasty-scans link malformed! If this is not a typo please contact Parmethyst#7954.',ephemeral=True)
+        return
+
+      await interaction.response.defer(ephemeral=False)
+      
+      if link[-1]=='/': #if last character of link is slash then remove slash
+        link=link[:-1]
+
+      split_link=link.rsplit('/',2)[1:] #split link to parts divided by '/' character
+      # print(split_link)
+
+      if split_link[0]=="chapters":
+        requested_page=1
+        chapter_permalink=""
+        if re.match(r"([a-z\-0-9\-_])+#([0-9])+", split_link[1]):
+            chapter_split=split_link[1].split('#')
+            chapter_permalink=chapter_split[0]
+            requested_page=int(chapter_split[1])
+            if(requested_page<1): requested_page=1
+        else:
+            chapter_permalink=split_link[1]
+        json_link=f"https://dynasty-scans.com/chapters/{chapter_permalink}.json"
+        response = requests.get(f"{json_link}")
+        json_data = json.loads(response.text)
+        # print(json_data)
+        if requested_page >= len(json_data["pages"]):
+            requested_page=len(json_data["pages"])
+        title=json_data["title"]
+        tags=json_data["tags"]
+        for tag in tags:
+            if tag["name"]=="NSFW":
+                is_nsfw=True
+            if tag["type"]=="Anthology" or tag["type"]=="Issue":
+                title=f"{json_data['title']} ({tag['name']})"
+            elif tag["type"]=="Doujin":
+                title=f"{json_data['title']} (Parody: {tag['name']})"
+        page_image=json_data["pages"][requested_page-1]["url"]
+        embed=discord.Embed(
+            title=f"{title}",
+            url=f"{link}")
+        embed.add_field(name="Preview doesn't load?", value=f"[https://dynasty-scans.com{page_image}](https://dynasty-scans.com{page_image})")
+        # embed.set_author(
+        #     name=ctx.author.display_name,
+        #     icon_url=ctx.author.avatar_url)
+        embed.set_image(url=f"https://dynasty-scans.com{page_image}")
+        embed.set_footer(text="Code: https://github.com/Parmethyst/MdexPreviewBot")
+        print(f"{title}")
+        await interaction.followup.send(embed=embed)
+
+      elif split_link[0]=="series" or split_link[0]=="anthologies" or split_link[0]=="issues":
+        is_nsfw=False
+        is_tags=False
+        is_cover=True
+        response = requests.get(f"{link}.json")
+        json_data = json.loads(response.text)
+        # print(json_data)
+        title=json_data["name"]
+        description=""
+        if json_data["description"] is None:
+            description=""
+        else:
+            description=json_data["description"]
+        cover_url=""
+        cover_link=""
+        if json_data['cover'] is None:
+            is_cover=False
+        else:
+            cover_url=json_data['cover'].partition("?")
+            cover_link=f"https://dynasty-scans.com{cover_url[0]}"
+            is_cover=True
+
+        tags=json_data["tags"]
+        tags.sort(key=lambda x: x["type"], reverse=False)
+        tag_list=list()
+        for tag in tags:
+            is_tags=True
+            tag_list.append(tag["name"])
+            if tag["name"]=="NSFW":
+                is_nsfw=True
+        tag_string=", ".join(tag_list)
+        embed=discord.Embed(  
+            title=f"{title}",
+            url=f"{link}",
+            description=f"{description}")
+        if is_tags: embed.add_field(name="Tags", value=f"{tag_string}")
+        # embed.set_author(
+        #     name=ctx.author.display_name,
+        #     icon_url=ctx.author.avatar_url)
+        if is_cover:
+            if not is_nsfw:
+                embed.set_image(url=f"{cover_link}")
+            # elif is_nsfw and ctx.channel.is_nsfw():
+            #     embed.set_image(url=f"{cover_link}")
+        embed.set_footer(text="Code: https://github.com/Parmethyst/MdexPreviewBot")
+        print(f"{title}")
+        await interaction.followup.send(embed=embed)
+
+async def setup(bot: commands.Bot) -> None:
+  await bot.add_cog(dynasty_module(bot))
--- main.py
+++ main.py
@@ -1,281 +1,106 @@
 # bot.py
 import os
-import requests
 import discord
-import json
-import re
-import time
+import asyncio
+import typing
+from typing import *
 from dotenv import load_dotenv
-from discord_slash import SlashCommand
 from discord.ext import commands
-from discord_slash.utils.manage_commands import create_option
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = commands.Bot(command_prefix = 'md.')
-slash = SlashCommand(client, sync_commands=True) # Declares slash commands through the client.
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.message_content = True
+
+client = commands.Bot(command_prefix='$', intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
 @client.event
-async def on_slash_command_error(ctx, error):
+async def close():
+    print("Shutting down...")
+    pass
 
-    if isinstance(error, discord.ext.commands.errors.MissingPermissions):
-        await ctx.send('You do not have permission to execute this command', hidden=True)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$try_sync') and message.guild.id==286355885389774868:
+        guild = discord.Object(id=286355885389774868)
+        client.tree.copy_global_to(guild=guild)
+        await client.tree.sync()
+        await message.channel.send("SYNC")
 
-    else:
-        await ctx.send('Oops! Something went wrong, please try again or report to Parmethyst#7954 if the issue persists.', hidden=True)
-        raise error  # this will show some debug print in the console, when debugging
 
-@slash.slash(name="ping")
-async def _ping(ctx):
-    await ctx.send(f"Pong! ({client.latency*1000}ms)")
+@client.command(name="sync") #NOT WORKING BUT INTERESTING
+@commands.guild_only()
+async def sync(
+        ctx: commands.Context, guilds: commands.Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    print("ENTER")
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
 
-@slash.slash( #Make a slash command
-    name="ds",
-    description="Command to preview dynasty-scans link",
-    options=[
-        create_option(
-            name="link",
-            description="Put your dynasty-scans link here",
-            option_type=3,
-            required=False
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
         )
-    ])
-async def ds(ctx, link="help"): #Slash command function
-    link=link.lower()
-    ts = time.localtime()
-    timeString=time.strftime("%Y-%m-%d %H:%M:%S", ts)
-    print(f"[{timeString}] {ctx.author.display_name}: {link}")
-    if link=="help":
-        print("Usage help successfully sent")
-        await ctx.send(content=f"Usage: /ds [dynasty-scans link without the brackets]")
         return
-    if not re.match(r"(https:\/\/dynasty-scans.com\/)(series|anthologies|issues|chapters)\/([a-z\-0-9\-_])+", link):
-        print("Not a dynasty-scans link!")
-        await ctx.send('dynasty-scans link malformed! If this is not a typo please contact Parmethyst#7954.', hidden=True)
-        return
-    await ctx.defer()
-    if link[-1]=='/': #if last character of link is slash then remove slash
-        link=link[:-1]
-    split_link=link.rsplit('/',2)[1:] #split link to parts divided by '/' character
 
-    if split_link[0]=="chapters":
-        requested_page=1
-        chapter_permalink=""
-        if re.match(r"([a-z\-0-9\-_])+#([0-9])+", split_link[1]):
-            chapter_split=split_link[1].split('#')
-            chapter_permalink=chapter_split[0]
-            requested_page=int(chapter_split[1])
-            if(requested_page<1): requested_page=1
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
         else:
-            chapter_permalink=split_link[1]
-        json_link=f"https://dynasty-scans.com/chapters/{chapter_permalink}.json"
-        response = requests.get(f"{json_link}")
-        json_data = json.loads(response.text)
-        if requested_page >= len(json_data["pages"]):
-            requested_page=len(json_data["pages"])
-        title=json_data["title"]
-        tags=json_data["tags"]
-        for tag in tags:
-            if tag["name"]=="NSFW":
-                is_nsfw=True
-            if tag["type"]=="Anthology" or tag["type"]=="Issue":
-                title=f"{json_data['title']} ({tag['name']})"
-            elif tag["type"]=="Doujin":
-                title=f"{json_data['title']} (Parody: {tag['name']})"
-        page_image=json_data["pages"][requested_page-1]["url"]
-        embed=discord.Embed(
-            title=f"{title}",
-            url=f"{link}")
-        embed.add_field(name="Preview doesn't load?", value=f"[https://dynasty-scans.com{page_image}](https://dynasty-scans.com{page_image})")
-        embed.set_author(
-            name=ctx.author.display_name, 
-            icon_url=ctx.author.avatar_url)
-        embed.set_image(url=f"https://dynasty-scans.com{page_image}")
-        embed.set_footer(text="Code: https://github.com/Parmethyst/MdexPreviewBot")
-        print(f"{title}")
-        await ctx.send(embed=embed)
+            ret += 1
 
-    elif split_link[0]=="series" or split_link[0]=="anthologies" or split_link[0]=="issues":
-        is_nsfw=False
-        is_tags=False
-        is_cover=True
-        response = requests.get(f"{link}.json")
-        json_data = json.loads(response.text)
-        title=json_data["name"]
-        description=""
-        if json_data["description"] is None:
-            description=""
-        else:
-            description=json_data["description"]
-        cover_url=""
-        cover_link=""
-        if json_data['cover'] is None:
-            is_cover=False
-        else:
-            cover_url=json_data['cover'].partition("?")
-            cover_link=f"https://dynasty-scans.com{cover_url[0]}"
-            is_cover=True
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
 
-        tags=json_data["tags"]
-        tags.sort(key=lambda x: x["type"], reverse=False)
-        tag_list=list()
-        for tag in tags:
-            is_tags=True
-            tag_list.append(tag["name"])
-            if tag["name"]=="NSFW":
-                is_nsfw=True
-        tag_string=", ".join(tag_list)
-        embed=discord.Embed(
-            title=f"{title}",
-            url=f"{link}",
-            description=f"{description}")
-        if is_tags: embed.add_field(name="Tags", value=f"{tag_string}")
-        embed.set_author(
-            name=ctx.author.display_name, 
-            icon_url=ctx.author.avatar_url)
-        if is_cover:
-            if not is_nsfw:
-                embed.set_image(url=f"{cover_link}")
-            elif is_nsfw and ctx.channel.is_nsfw():
-                embed.set_image(url=f"{cover_link}")
-        embed.set_footer(text="Code: https://github.com/Parmethyst/MdexPreviewBot")
-        print(f"{title}")
-        await ctx.send(embed=embed)
+
+# Works like:
+# !sync -> global sync
+# !sync ~ -> sync current guild
+# !sync * -> copies all global app commands to current guild and syncs
+# !sync ^ -> clears all commands from the current guild target and syncs (removes guild commands)
+# !sync id_1 id_2 -> syncs guilds with id 1 and 2
+
+
+async def load():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await client.load_extension(f'cogs.{filename[:-3]}')
+
+
+async def main():
+    await load()
+    async with client:
+        await client.start(TOKEN)
+
+
+async def shutdown_handler():
+    await client.close()
 
 
 
-@slash.slash( #Make a slash command
-    name="md",
-    description="Command to preview a mangadex link",
-    options=[
-        create_option(
-            name="link",
-            description="Put your mangadex link here",
-            option_type=3,
-            required=False
-        )
-    ])
-async def md(ctx, link="help"): #Slash command function
-    link=link.lower()
-    ts = time.localtime()
-    timeString=time.strftime("%Y-%m-%d %H:%M:%S", ts)
-    print(f"[{timeString}] {ctx.author.display_name}: {link}")
-    if link=="help":
-        print("Usage help successfully sent")
-        await ctx.send(content=f"Usage: /md [mangadex manga/chapter link without the brackets]")
-        return
-    if not re.match(r"(https:\/\/mangadex.org\/)(title|chapter)\/([a-z\-0-9])+", link):
-        print("Not a mangadex link!")
-        await ctx.send('MangaDex link malformed! If this is not a typo please contact Parmethyst#7954.', hidden=True)
-        return
-    await ctx.defer()
-    if link[-1]=='/': #if last character of link is slash then remove slash
-        link=link[:-1]
-    split_link=link.rsplit('/',2)[1:] #split link to parts divided by '/' character
-    if split_link[0]=="title":
-
-        manga_id=split_link[1]
-        response = requests.get(f"https://api.mangadex.org/manga/{manga_id}")
-        json_data = json.loads(response.text)
-        title=get_suitable_title_language(json_data)
-        description=json_data["data"]["attributes"]["description"]["en"]
-        content_rating=json_data["data"]["attributes"]["contentRating"]
-        if content_rating=="pornographic":
-            title=f"{title} (NSFW)"
-        cover_id=""
-        relationships=json_data["relationships"]
-
-        for relation in relationships:
-            if relation["type"]=="cover_art":
-                cover_id=relation["id"]
-                break
-
-        response = requests.get(f"https://api.mangadex.org/cover/{cover_id}")
-        json_data = json.loads(response.text)
-        cover_filename=json_data["data"]["attributes"]["fileName"]
-
-        embed=discord.Embed(
-            title=f"{title}",
-            url=f"{link}",
-            description=f"{description}")
-
-        embed.set_author(
-            name=ctx.author.display_name, 
-            icon_url=ctx.author.avatar_url)
-        if not content_rating=="pornographic":
-            embed.set_image(url=f"https://uploads.mangadex.org/covers/{manga_id}/{cover_filename}")
-        elif content_rating=="pornographic" and ctx.channel.is_nsfw():
-            embed.set_image(url=f"https://uploads.mangadex.org/covers/{manga_id}/{cover_filename}")
-        embed.set_footer(text="Code: https://github.com/Parmethyst/MdexPreviewBot")
-        print(title)
-        await ctx.send(embed=embed)
-
-    else:
-        chapter_id=''
-        requested_page=0
-        if split_link[0]=="chapter":
-            chapter_id=split_link[1]
-        else:
-            chapter_id=split_link[0]
-            requested_page=int(split_link[1])
-
-        if requested_page <= 0:
-            requested_page=0
-        else:
-            requested_page=int(split_link[1])-1
-
-        response = requests.get(f"https://api.mangadex.org/chapter/{chapter_id}")
-        json_data=json.loads(response.text)
-
-        if requested_page >= len(json_data["data"]["attributes"]["data"]):
-            requested_page=len(json_data["data"]["attributes"]["data"])-1
-        
-        chapter_hash=json_data["data"]["attributes"]["hash"]
-        chapter_page=json_data["data"]["attributes"]["data"][requested_page]
-        chapter_number=json_data["data"]["attributes"]["chapter"]
-        relationships=json_data["relationships"]
-        response = requests.get(f"https://api.mangadex.org/at-home/server/{chapter_id}")
-        json_data=json.loads(response.text)
-        base_url=json_data["baseUrl"]
-
-        manga_id=""
-        for relation in relationships:
-            if relation["type"]=="manga":
-                manga_id=relation["id"]
-                break
-
-        response = requests.get(f"https://api.mangadex.org/manga/{manga_id}")
-        json_data = json.loads(response.text)
-        title=get_suitable_title_language(json_data)
-
-        embed=discord.Embed(
-            title=f"{title} (Ch: {chapter_number})",
-            url=f"{link}")
-        embed.set_author(
-            name=ctx.author.display_name, 
-            icon_url=ctx.author.avatar_url)
-        embed.set_image(url=f"{base_url}/data/{chapter_hash}/{chapter_page}")
-        embed.set_footer(text="Code: https://github.com/Parmethyst/MdexPreviewBot")
-        print(f"{title} (Ch: {chapter_number})")
-        await ctx.send(embed=embed)
-
-def get_suitable_title_language(json_data):
-    if "en" in json_data["data"]["attributes"]["title"]:
-        return json_data["data"]["attributes"]["title"]["en"]
-    elif "jp" in json_data["data"]["attributes"]["title"]:
-        return json_data["data"]["attributes"]["title"]["jp"]
-    elif "kr" in json_data["data"]["attributes"]["title"]:
-        return json_data["data"]["attributes"]["title"]["kr"]
-    elif "zh-hk" in json_data["data"]["attributes"]["title"]:
-        return json_data["data"]["attributes"]["title"]["zh-hk"]
-    elif "ru" in json_data["data"]["attributes"]["title"]:
-        return json_data["data"]["attributes"]["title"]["ru"]
-    else:
-        return "N/A"
-
-client.run(TOKEN)
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    pass
+finally:
+    print("Shutting down...")
